ee64e142c35d0f0e3621bc15a0aa4acc86bed579
vjunloc <vijaytyagi.vt@gmail.com>
2015-06-09 21:58:16 +0530

    updated the store

---
+++ constants/HNConstants.js
@@ -0,0 +1,9 @@
+
+var keyMirror = require("keymirror");
+
+var HNConstants = keyMirror({
+	LOAD_MORE_STORIES: null,
+	LOAD_MORE_COMMENTS: null
+});
+
+module.exports = HNConstants;
--- stores/TopStory.js
+++ stores/TopStory.js
@@ -1,18 +1,28 @@
 var {EventEmitter} = require("events"),
-	HNDispatcher = require("../dispatcher/HNDispatcher");
+	HNDispatcher = require("../dispatcher/HNDispatcher"),
+	assign = require("object-assign"),
+	HNConstants = require("../constants/HNConstants");
 
 const CHANGE_EVENT = "change";
+var _topStories = {},
+	_topStoryIds = [];
 
 //Todo register the store with dispatcher
-class TopStory extends EventEmitter {
-	constructor(){
-		this._stories = [];
-	}
-
-	emitChange(){
+var TopStory = assign({}, EventEmitter.prototype, {
+	
+	emitChange: function(){
 		this.emit(CHANGE_EVENT);
+	},
+	getStories: function(){
+
+	},
+	addEventListener: function(callback){
+		this.on(CHANGE_EVENT, callback)
+	},
+	removeEventListener: function(callback){
+		this.removeListener(CHANGE_EVENT, callback);
 	}
-}
+});
 
 HNDispatcher.register(function(action){
 
