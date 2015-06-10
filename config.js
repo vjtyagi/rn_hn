0192d1b9fc688fcdcc207c487d4609966b37f9db
vjunloc <vijaytyagi.vt@gmail.com>
2015-06-10 21:16:51 +0530

    removed the files from root

--- config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-	API_HOST: "http://api.hackernewsmobile.com/",
-	PAGE_SIZE: 10,
-	threshold: 20,
-	ITEM_URL: "https://hacker-news.firebaseio.com/v0/item/",
-	TOPSTORIES_URL: "https://hacker-news.firebaseio.com/v0/topstories.json"
-};
-module.exports = config;
--- util.js
+++
@@ -1,9 +0,0 @@
-var config = require("./config");
-var URL = require('url');
-
-module.exports = {
-	getHostName: function(url){
-		return URL.parse(url).hostname;
-
-	}
-}
