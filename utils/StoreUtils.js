1e4ebea01265ec442c582cef1f4b550661cee1fc
vjunloc <vijaytyagi.vt@gmail.com>
2015-06-13 19:34:49 +0530

    Added store utils

---
+++ utils/StoreUtils.js
@@ -0,0 +1,28 @@
+
+var {EventEmitter} = require('events'),
+	assign = require("object-assign");
+
+var CHANGE_EVENT = "change";
+
+var StoreUtils = {
+
+	createStore: function ( spec ) {
+
+		var emitter = new EventEmitter();
+		var store = assign({
+			emitChange: function () {
+				emitter.emit( CHANGE_EVENT );
+			},
+			addEventListener: function ( callback ) {
+				emitter.on( CHANGE_EVENT, callback );
+			},
+			removeEventListener: function ( callback ) {
+				emitter.removeListener( CHANGE_EVENT, callback) ;
+			}
+		}, spec);
+
+		return store;
+	}
+};
+
+module.exports  = StoreUtils;
