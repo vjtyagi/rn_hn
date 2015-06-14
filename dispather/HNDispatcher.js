8f30fc644868c5ac49670d3231746d4a4eabb743
vjunloc <vijaytyagi.vt@gmail.com>
2015-06-14 07:47:06 +0530

    Added async dispatch

--- dispather/HNDispatcher.js
+++ dispather/HNDispatcher.js
@@ -1,4 +1,20 @@
 var {Dispatcher} = require("flux"),
 	dispatcherInstance = new Dispatcher();
 
+function dispatchAsync(promise, types, action) {
+	var {request, success, failure} = types
+
+	action = action || {};
+
+	dispatcherInstance.dispatch(request, action)
+
+	promise.then(
+		response => dispatcherInstance.dispatch(success, {...action, response}),
+		error => dispatcherInstance.dispatch(failure, {...action, error})
+
+	);
+}
+
+dispatcherInstance.dispatchAsync = dispatchAsync;
+
 module.exports = dispatcherInstance;
